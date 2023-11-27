b998ecc286ac8447157e57212cac7a2bc8f34b07
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2023-11-27 13:32:32 -0500

    removed individual fibers, added load_fiber [publish]

--- FiberFusing/fiber/catalogue.py
+++ FiberFusing/fiber/catalogue.py
@@ -16,33 +16,31 @@ fiber.plot().show()
 
 """
 
-
 from FiberFusing.fiber.base_class import GenericFiber
 from FiberFusing import micro
 from MPSTools.fiber_catalogue.loader import load_fiber_as_dict
 from MPSTools.material_catalogue.loader import get_silica_index
 
 __all__ = [
-    'SMF28',
-    'HP630',
-    'HI1060',
     'CapillaryTube',
-    'FluorineCapillaryTube',
-    'FluorineCapillaryTube_2',
     'GradientCore',
-    'DCF13',
     'CustomFiber',
-    'DCF1300S_20',
-    'DCF1300S_33',
-    'DCF1300S_26',
-    'DCF1300S_42',
-    'F2058L1',
-    'F2058G1',
-    'F2028M24',
-    'F2028M21',
-    'F2028M12',
 
 ]
+
+
+def load_fiber(fiber_name: str, wavelength: float) -> GenericFiber:
+    fiber = GenericFiber(wavelength=wavelength)
+
+    fiber_dict = load_fiber_as_dict(
+        fiber_name=fiber_name,
+        wavelength=wavelength
+    )
+
+    for _, structure in fiber_dict['layers'].items():
+        fiber.create_and_add_new_structure(**structure)
+
+    return fiber
 
 
 class CustomFiber(GenericFiber):
@@ -50,51 +48,6 @@ class CustomFiber(GenericFiber):
         super().__init__(wavelength=wavelength, position=position)
 
         self.add_air()
-
-
-class SMF28(GenericFiber):
-    model = "SMF28"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        fiber_dict = load_fiber_as_dict(
-            fiber_name=self.model,
-            wavelength=self.wavelength
-        )
-
-        for _, structure in fiber_dict['layers'].items():
-            self.create_and_add_new_structure(**structure)
-
-
-class HP630(GenericFiber):
-    model = "HP630"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        fiber_dict = load_fiber_as_dict(
-            fiber_name=self.model,
-            wavelength=self.wavelength
-        )
-
-        for _, structure in fiber_dict['layers'].items():
-            self.create_and_add_new_structure(**structure)
-
-
-class HI1060(GenericFiber):
-    model = "HI630"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        fiber_dict = load_fiber_as_dict(
-            fiber_name=self.model,
-            wavelength=self.wavelength
-        )
-
-        for _, structure in fiber_dict['layers'].items():
-            self.create_and_add_new_structure(**structure)
 
 
 class CapillaryTube(GenericFiber):
@@ -135,156 +88,5 @@ class GradientCore(GenericFiber):
             radius=core_radius,
             index=self.pure_silica_index
         )
-
-
-class DCF13(GenericFiber):
-    model = "DCF13"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        fiber_dict = load_fiber_as_dict(
-            fiber_name=self.model,
-            wavelength=self.wavelength
-        )
-
-        for _, structure in fiber_dict['layers'].items():
-            self.create_and_add_new_structure(**structure)
-
-
-class DCF1300S_20(GenericFiber):
-    model = "DCF1300S_20"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        fiber_dict = load_fiber_as_dict(
-            fiber_name=self.model,
-            wavelength=self.wavelength
-        )
-
-        for _, structure in fiber_dict['layers'].items():
-            self.create_and_add_new_structure(**structure)
-
-
-class DCF1300S_33(GenericFiber):
-    model = "DCF1300S_33"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        fiber_dict = load_fiber_as_dict(
-            fiber_name=self.model,
-            wavelength=self.wavelength
-        )
-
-        for _, structure in fiber_dict['layers'].items():
-            self.create_and_add_new_structure(**structure)
-
-
-class DCF1300S_26(GenericFiber):
-    model = "DCF1300S_26"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        fiber_dict = load_fiber_as_dict(
-            fiber_name=self.model,
-            wavelength=self.wavelength
-        )
-
-        for _, structure in fiber_dict['layers'].items():
-            self.create_and_add_new_structure(**structure)
-
-
-class DCF1300S_42(GenericFiber):
-    model = "DCF1300S_42"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        fiber_dict = load_fiber_as_dict(
-            fiber_name=self.model,
-            wavelength=self.wavelength
-        )
-
-        for _, structure in fiber_dict['layers'].items():
-            self.create_and_add_new_structure(**structure)
-
-
-class F2058L1(GenericFiber):
-    model = "F2058L1"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        fiber_dict = load_fiber_as_dict(
-            fiber_name=self.model,
-            wavelength=self.wavelength
-        )
-
-        for _, structure in fiber_dict['layers'].items():
-            self.create_and_add_new_structure(**structure)
-
-
-class F2058G1(GenericFiber):
-    model = "F2058G1"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        fiber_dict = load_fiber_as_dict(
-            fiber_name=self.model,
-            wavelength=self.wavelength
-        )
-
-        for _, structure in fiber_dict['layers'].items():
-            self.create_and_add_new_structure(**structure)
-
-
-class F2028M24(GenericFiber):
-    model = "F2028M24"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        fiber_dict = load_fiber_as_dict(
-            fiber_name=self.model,
-            wavelength=self.wavelength
-        )
-
-        for _, structure in fiber_dict['layers'].items():
-            self.create_and_add_new_structure(**structure)
-
-
-class F2028M21(GenericFiber):
-    model = "F2028M21"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        fiber_dict = load_fiber_as_dict(
-            fiber_name=self.model,
-            wavelength=self.wavelength
-        )
-
-        for _, structure in fiber_dict['layers'].items():
-            print(structure)
-            self.create_and_add_new_structure(**structure)
-
-
-class F2028M12(GenericFiber):
-    model = "F2028M12"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        fiber_dict = load_fiber_as_dict(
-            fiber_name=self.model,
-            wavelength=self.wavelength
-        )
-
-        for _, structure in fiber_dict['layers'].items():
-            self.create_and_add_new_structure(**structure)
 
 # -
