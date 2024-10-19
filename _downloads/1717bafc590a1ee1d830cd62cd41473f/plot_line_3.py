8176eef3f303ade75981f1cac432ade5f154ac77
MartinPdeS <MartinPdeS@users.noreply.github.com>
2024-10-19 12:33:29 +0000

    deploy: c7c20462a029c7e84dc4ba32b5ecdd1d11d0ebe0

---
+++ _downloads/1717bafc590a1ee1d830cd62cd41473f/plot_line_3.py
@@ -0,0 +1,18 @@
+"""
+3x3 Line - Clad
+===============
+"""
+
+from FiberFusing.configuration.line import FusedProfile_03x03 as FusedProfile
+
+
+clad = FusedProfile(
+    fiber_radius=62.5e-6,
+    fusion_degree=0.3,
+    index=1.4444,
+    core_position_scrambling=0
+)
+
+clad.plot()
+
+# -
---
+++ _downloads/d47e9341ee684cc47a0eb76284e322be/plot_geometry_01.py
@@ -0,0 +1,35 @@
+"""
+1x1 Geometry
+============
+"""
+
+from FiberFusing import Geometry, BackGround
+from FiberFusing.fiber.catalogue import load_fiber, get_silica_index
+from FiberFusing.configuration.ring import FusedProfile_01x01 as FusedProfile
+
+wavelength = 1.55e-6
+
+air = BackGround(index=1.0)
+
+clad = FusedProfile(
+    fiber_radius=62.5e-6,
+    index=get_silica_index(wavelength=wavelength)
+)
+
+fibers = [
+    load_fiber('SMF28', wavelength=wavelength, position=core) for core in clad.cores
+]
+
+geometry = Geometry(
+    background=air,
+    additional_structure_list=[clad],
+    x_bounds='centering',
+    y_bounds='centering',
+    resolution=250
+)
+
+_ = geometry.add_fiber(*fibers)
+
+geometry.plot()
+
+# -
